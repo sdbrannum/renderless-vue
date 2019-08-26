2ac38cbd95e14a36f795c7ce7ac1afd6b5b42828
Steven Brannum <sdbrannum@gmail.com>
2019-08-25 22:39:05 -0500

    Extract search to separate file, minimum window subsequence implementation

--- src/components/FuzzySearch.worker.js
+++ src/components/FuzzySearch.worker.js
@@ -1,153 +1,52 @@
+import Search from './Search';
+
 const MSG_TYPES = {
     config: 'CONFIG',
     data: 'DATA',
     search: 'SEARCH',
 };
 
-const rankings = {
-    CASE_SENSITIVE_EQUAL: 9,
-    EQUAL: 8,
-    STARTS_WITH: 7,
-    WORD_STARTS_WITH: 6,
-    STRING_CASE: 5,
-    STRING_CASE_ACRONYM: 4,
-    CONTAINS: 3,
-    ACRONYM: 2,
-    LCS: 1,
-    NO_MATCH: 0,
-};
-
-// https://gist.github.com/nmsdvid/8807205
-// const debounce = (callback, time = 250, interval) => (...args) => {
-//     clearTimeout(interval);
-//     interval = setTimeout(() => {
-//         interval = null;
-//         callback(...args);
-//     }, time);
-// };
-
-class Search {
-    /**
-     *
-     * @param {Object} options
-     * @param {Boolean} paged
-     * @param {Number} page
-     */
+class Comms {
     constructor(options) {
-        this.page = 1;
+        this.search = new Search();
+        console.log('search >>', this.search);
         this.data = [];
         this.results = [];
         if (options && options.paged) this.paged = options.paged;
-    }
-    previousPage() {}
-    nextPage() {}
-    execute(query) {
-        this.results = [];
-        // TODO: rank so we can sort
-        console.log('hit search >', query);
-        if (!query) {
-            return this.data;
-        }
-
-        const QUERY = query;
-        const QUERY_UPPER = query.toUpperCase();
-
-        for (let i = 0; i < this.data.length; i++) {
-            if (this.results.length > 2500) break;
-            const el = this.data[i];
-            // case sensitive equals
-            if (el.question === QUERY) {
-                this.results.push({
-                    rank: rankings.CASE_SENSITIVE_EQUAL,
-                    rankedItem: el.question,
-                });
-                continue;
-            }
-            // upper casing before further comparison
-            const EL_UPPER = el.question.toUpperCase();
-            console.log('EL_UPPER >>', EL_UPPER);
-            console.log('QUERY_UPPER >>', QUERY_UPPER);
-
-            // case insensitive equals
-            if (EL_UPPER === QUERY_UPPER) {
-                this.results.push({
-                    rank: rankings.EQUAL,
-                    rankedItem: el.question,
-                });
-                continue;
-            }
-
-            // starts with
-            if (EL_UPPER.indexOf(QUERY_UPPER) === 0) {
-                this.results.push({
-                    rank: rankings.STARTS_WITH,
-                    rankedItem: el.question,
-                });
-                continue;
-            }
-
-            // any word starts with
-            // TODO: not correct
-            if (EL_UPPER.indexOf(QUERY_UPPER) !== -1) {
-                this.results.push({
-                    rank: rankings.WORD_STARTS_WITH,
-                    rankedItem: el.question,
-                });
-                continue;
-            }
-
-            // in order subsequence
-            const lcs = longestCommonSubsequence(EL_UPPER, QUERY_UPPER);
-            if (lcs.length > 0 && lcs[0] !== '') {
-                this.results.push({
-                    rank: rankings.LCS,
-                    rankedItem: el.question,
-                });
-                continue;
-            }
-        }
-
-        console.log('Total results length: ', this.results.length);
-        this.sortRankedItems();
-        if (this.paged) {
-            const pageRangeLower = this.paged ? this.page * 5 - 5 : 0;
-            sendMessage('RESULTS', this.results.slice(pageRangeLower, 5));
-        } else {
-            sendMessage('RESULTS', this.results);
-        }
-    }
-    removeAccents(str) {
-        return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-    }
-    sortRankedItems() {
-        // todo: sort same ranked items alphabetially
-        this.results.sort((a, b) => b.rank - a.rank);
-    }
-}
-
-class Communicator {
-    constructor() {
-        this.search = new Search();
+        this.page = 1;
     }
 
-    sendResults() {
-        self.postMessage('results');
+    sendMessage(type, payload) {
+        postMessage(JSON.stringify({ type, payload }));
     }
+
     handleMessage(e) {
         console.log('handling message');
         const msg = JSON.parse(e.data);
 
         switch (msg.type.toString()) {
             case MSG_TYPES.config:
-                this.search.paged = msg.payload.paged;
+                this.paged = msg.payload.paged;
                 break;
             case MSG_TYPES.data:
                 console.log('hit data case');
-                this.search.data = msg.payload;
+                this.data = msg.payload;
                 break;
             case MSG_TYPES.search:
-                console.log('hit search case');
-                this.search.execute(msg.payload);
+                console.log('hit search case', this.search);
+                console.log(msg.payload);
+                console.log(this.data.length);
+                this.results = this.search.execute(msg.payload, this.data);
+                console.log(this.results);
+                if (this.paged) {
+                    const pageRangeLower = this.paged ? this.page * 5 - 5 : 0;
+                    this.sendMessage(
+                        'RESULTS',
+                        this.results.slice(pageRangeLower, 5)
+                    );
+                } else {
+                    this.sendMessage('RESULTS', this.results);
+                }
                 break;
             default:
                 break;
@@ -155,94 +54,7 @@ class Communicator {
     }
 }
 
-/**
- * @param {String} set1
- * @param {String} set2
- * @return {String}
- * @remarks https://github.com/trekhleb/javascript-algorithms/
- */
-// todo: save searches
-function longestCommonSubsequence(set1, set2) {
-    // Init LCS matrix.
-    const lcsMatrix = Array(set2.length + 1)
-        .fill(null)
-        .map(() => Array(set1.length + 1).fill(null));
-
-    // Fill first row with zeros.
-    for (let columnIndex = 0; columnIndex <= set1.length; columnIndex += 1) {
-        lcsMatrix[0][columnIndex] = 0;
-    }
-
-    // Fill first column with zeros.
-    for (let rowIndex = 0; rowIndex <= set2.length; rowIndex += 1) {
-        lcsMatrix[rowIndex][0] = 0;
-    }
-
-    // Fill rest of the column that correspond to each of two strings.
-    for (let rowIndex = 1; rowIndex <= set2.length; rowIndex += 1) {
-        for (
-            let columnIndex = 1;
-            columnIndex <= set1.length;
-            columnIndex += 1
-        ) {
-            if (set1[columnIndex - 1] === set2[rowIndex - 1]) {
-                lcsMatrix[rowIndex][columnIndex] =
-                    lcsMatrix[rowIndex - 1][columnIndex - 1] + 1;
-            } else {
-                lcsMatrix[rowIndex][columnIndex] = Math.max(
-                    lcsMatrix[rowIndex - 1][columnIndex],
-                    lcsMatrix[rowIndex][columnIndex - 1]
-                );
-            }
-        }
-    }
-
-    // Calculate LCS based on LCS matrix.
-    if (!lcsMatrix[set2.length][set1.length]) {
-        // If the length of largest common string is zero then return empty string.
-        return [''];
-    }
-
-    const longestSequence = [];
-    let columnIndex = set1.length;
-    let rowIndex = set2.length;
-
-    while (columnIndex > 0 || rowIndex > 0) {
-        if (set1[columnIndex - 1] === set2[rowIndex - 1]) {
-            // Move by diagonal left-top.
-            longestSequence.unshift(set1[columnIndex - 1]);
-            columnIndex -= 1;
-            rowIndex -= 1;
-        } else if (
-            lcsMatrix[rowIndex][columnIndex] ===
-            lcsMatrix[rowIndex][columnIndex - 1]
-        ) {
-            // Move left.
-            columnIndex -= 1;
-        } else {
-            // Move up.
-            rowIndex -= 1;
-        }
-    }
-
-    return longestSequence;
-}
-
-// Communicator.prototype.handleMessage = function(msg) {
-//     console.log('got message');
-// };
-
 (function() {
-    const comm = new Communicator();
-
+    const comm = new Comms();
     onmessage = e => comm.handleMessage(e);
 })();
-
-function sendMessage(type, payload) {
-    postMessage(JSON.stringify({ type, payload }));
-}
-
-// const x = new Communicator();
-// onmessage = function(e) {
-//     console.log('Message received from main script');
-// };
---
+++ src/components/Search.js
@@ -0,0 +1,161 @@
+const rankings = {
+    CASE_SENSITIVE_EQUAL: 9,
+    EQUAL: 8,
+    STARTS_WITH: 7,
+    WORD_STARTS_WITH: 6,
+    STRING_CASE: 5,
+    STRING_CASE_ACRONYM: 4,
+    CONTAINS: 3,
+    ACRONYM: 2,
+    SUBSEQUENCE: 1,
+    NO_MATCH: 0,
+};
+
+export default class Search {
+    constructor() {}
+    /**
+     * Return sorted and matched results based on given query and data
+     * @param {String} query
+     * @param {Array} data
+     * @remarks by reference
+     */
+    execute(query, data) {
+        if (!query) {
+            return data;
+        }
+
+        const results = [];
+        const QUERY = query;
+        const QUERY_UPPER = query.toUpperCase();
+
+        for (let i = 0; i < data.length; i++) {
+            if (results.length > 2500) break;
+            const el = data[i];
+            // case sensitive equals
+            if (el.question === QUERY) {
+                results.push({
+                    rank: rankings.CASE_SENSITIVE_EQUAL,
+                    rankedItem: el.question,
+                });
+                continue;
+            }
+
+            // upper casing before further comparison
+            const EL_UPPER = el.question.toUpperCase();
+
+            // case insensitive equals
+            if (EL_UPPER === QUERY_UPPER) {
+                results.push({
+                    rank: rankings.EQUAL,
+                    rankedItem: el.question,
+                });
+                continue;
+            }
+
+            // starts with
+            if (EL_UPPER.indexOf(QUERY_UPPER) === 0) {
+                results.push({
+                    rank: rankings.STARTS_WITH,
+                    rankedItem: el.question,
+                });
+                continue;
+            }
+
+            // any word except first starts with
+            if (EL_UPPER.indexOf(` ${QUERY_UPPER}`) !== -1) {
+                results.push({
+                    rank: rankings.WORD_STARTS_WITH,
+                    rankedItem: el.question,
+                });
+                continue;
+            }
+
+            // contains
+            if (EL_UPPER.indexOf(QUERY_UPPER) !== -1) {
+                results.push({
+                    rank: rankings.CONTAINS,
+                    rankedItem: el.question,
+                });
+            }
+
+            // in order subsequence
+            const x = this.minWinSeq(QUERY_UPPER, EL_UPPER);
+            if (x.length > 0) {
+                results.push({
+                    rank: rankings.SUBSEQUENCE,
+                    rankedItem: el.question,
+                    distance: x.length - QUERY_UPPER.length,
+                });
+                continue;
+            }
+        }
+
+        const sortRankedItems = (a, b) => {
+            if (a.rank === b.rank) {
+                if (a.rank === rankings.SUBSEQUENCE) {
+                    return a.distance - b.distance;
+                }
+                return a.rankedItem
+                    .toString()
+                    .localeCompare(b.rankedItem.toString());
+            }
+            return b.rank - a.rank;
+        };
+
+        results.sort(sortRankedItems);
+
+        return results;
+    }
+
+    /**
+     * Minimum window subsequence
+     * @param {String} query
+     * @param {String} strToRank
+     * @returns {String} minimum subsequence window
+     * @remarks https://www.youtube.com/watch?v=W2DvQcDPD9A
+     */
+    minWinSeq(query, strToRank) {
+        if (query.length < 1 || strToRank.length < 1) {
+            return '';
+        }
+        if (query.length > strToRank.length) {
+            return '';
+        }
+
+        let minSequence = '',
+            j = 0,
+            end = 0,
+            min = strToRank.length + 1;
+
+        for (let i = 0; i < strToRank.length; i++) {
+            if (strToRank[i] === query[j]) {
+                j += 1;
+                if (j >= query.length) {
+                    end = i + 1;
+                    j -= 1;
+                    while (j >= 0) {
+                        if (query[j] === strToRank[i]) {
+                            j -= 1;
+                        }
+                        i -= 1;
+                    }
+                    i += 1;
+                    j += 1;
+                    if (end - i < min) {
+                        min = end - i;
+                        minSequence = strToRank.substring(end, i);
+                    }
+                }
+            }
+        }
+        return minSequence;
+    }
+    /**
+     * Remove accents/diacritics from a given string
+     * @param {String} str
+     * @remarks https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript
+     */
+    removeDiacritics(str) {
+        return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+    }
+}
